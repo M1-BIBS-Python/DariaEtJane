b37efb3bf9ffe8de09564422ffc57c1d690df32d
kdeazevedo <kevin.de-azevedo@u-psud.fr>
2017-04-26 19:34:30 +0200

    Main du projet

--- ProjetDocking.py
+++ ProjetDocking.py
@@ -1,13 +1,18 @@
 from structureTools import parserPDB, preparePDB
+from RunScore import compEner
 import sys
 from path import path
 import re
 
-pathfile = raw_input("Entrez le chemin du dossier : ")
+#pathfile = raw_input("Entrez le chemin du dossier : ")
 
-for files in path(pathfile).walkfiles():
-	if re.match(pathfile+"1BRS(.)*", files):
-		dicoPDB = parserPDB(files)
-		newdicoPDB = preparePDB(dicoPDB)
+dico_Rec = preparePDB(sys.argv[1])
+dico_Scores = {}
+
+for files in path("/home/kazevedo/Documents/M1BIBS/S2/Python/Python2016/Docking/sample").walkfiles():
+	if re.match("/home/kazevedo/Documents/M1BIBS/S2/Python/Python2016/Docking/sample/1BRS(.)*", files):
+		dico_confH = preparePDB(files)
+		dico_Scores[files]= compEner(dico_Rec, dico_confH, "B", "D")
+		dico_confH = {}
 	
-print dicoPDB
+print dico_Scores
