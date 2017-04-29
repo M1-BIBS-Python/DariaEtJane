680c5dfb8b356bf17777e9388610c368bdcb7648
kdeazevedo <kevin.de-azevedo@u-psud.fr>
2017-04-29 19:13:08 +0200

    Ajout de l'écritire d'un nouveau fichier PDB

--- ProjetDocking.py
+++ ProjetDocking.py
@@ -1,21 +1,32 @@
-from structureTools import parserPDB, preparePDB, scorelist
+from structureTools import parserPDB, preparePDB, scorelist, writePDB
 from RunScore import compEner
 import sys
 from path import path
 import re
 
-#pathfile = raw_input("Entrez le chemin du dossier : ")
+#pathfile = raw_input("Path of the repertory containing all the conformations : ")
 dico_Rec = preparePDB(sys.argv[1])
 dico_Scores = {}
 n=0
+listFiles=[]
+listScores=[]
 
 for files in path("/home/kazevedo/Documents/M1BIBS/S2/Python/Python2016/Docking/sample").walkfiles():
 	if re.match("/home/kazevedo/Documents/M1BIBS/S2/Python/Python2016/Docking/sample/1BRS(.)*", files):
+		n+=1
 		dico_confH = preparePDB(files)
-		dico_Scores[files]= compEner(dico_Rec, dico_confH, "B", "D")
+		listFiles.append(n)
+		listScores.append(compEner(dico_Rec, dico_confH, "B", "D"))
 		dico_confH = {}
 
-scorelist(dico_Scores)
-bestScore = float(max(dico_Scores.values()))
-
+print listFiles
+print listScores
+scorelist(listFiles, listScores)
+bestScore = float(max(listScores))
 print bestScore
+
+#pathfile = raw_input("Path of the PDB file giving the best score : ")
+
+dico_Lig = preparePDB("/home/kazevedo/Documents/M1BIBS/S2/Python/Python2016/Docking/sample/1BRS_A_1BRS_B_allatom_1_DP.pdb")
+writePDB(dico_Rec, dico_Lig, prediction = "complexe_predit_score1.pdb")
+
