2b378c16535867f6f1762de1832c01ca46ccc582
kdeazevedo <kevin.de-azevedo@u-psud.fr>
2017-05-01 22:44:06 +0200

    Compute the Interface

--- Interface.py
+++ Interface.py
@@ -3,7 +3,9 @@
 
 import sys
 from math import sqrt
-from structureTools import distancePoints, centerMassOfResidue, computeDist_dico
+from StructureTools import distancePoints, centerMassOfResidue, computeDist_dico
+from ResiduesProperties import hydrophobicResidue
+from parsing import parserPDB
 
 
 
@@ -11,12 +13,39 @@ def initBfactor(dPDB) :
 	for chain in dPDB["nchaine"]:
 		for res in dPDB[chain]["position"]:
 			dPDB[chain][res]["bfactor"] = 0
+
+
+def computeInterfaceScore(dPDB1, dPDB2, threshold, mode) :
+    
+	initBfactor(dPDB1)
+	initBfactor(dPDB2)
+    
+	nbContactHH = 0
+	nbResInterface = 0
+	percentage = 0
+    
+	for chain1 in dPDB1 :
+		if not chain1 == "nchaine":
+			for res1 in dPDB1[chain1]["position"] :
+				for chain2 in dPDB2 :
+					if not chain2 == "nchaine" :
+						for res2 in dPDB2[chain2]["position"] :
+							#Calculate the distance between all residues
+							distance = computeDist_dico(dPDB1[chain1][res1], dPDB2[chain2][res2], mode = mode)
+							if not distance > threshold : # means, the two residues belong to the interface							
+								nbResInterface += 1
+								if ((hydrophobicResidue(dPDB1[chain1][res1]['residu'])) and not(hydrophobicResidue(dPDB2[chain2][res2]['residu']))) or (not(hydrophobicResidue(dPDB1[chain1][res1]['residu'])) and hydrophobicResidue(dPDB2[chain2][res2]['residu'])):
+									nbContactHH += 1
+									
+	if not nbResInterface == 0:
+		percentage = (nbContactHH)/(nbResInterface)		
+	
+	return percentage
     
 
 def computeInterface(dPDB, threshold, mode) :
     
     initBfactor(dPDB)
-    
     
     for chain1 in dPDB :
 		if not chain1 == "nchaine":
@@ -36,13 +65,16 @@ def computeInterface(dPDB, threshold, mode) :
 									dPDB[chain1][resi]["bfactor"] = 1
 								if dPDB[chain2][resj]["bfactor"] == 0 :
 									dPDB[chain2][resj]["bfactor"] = 1
+						
 																					
 							#Else the two residues doesn't belong to the interface --> bfactor = 0
+
 
 
 def compareInterface (dPDB1, dPDB2) :
 	
 	nbInter = 0
+
 	
 	for chain in dPDB1 :
 		if not chain == "nchaine":
@@ -52,11 +84,16 @@ def compareInterface (dPDB1, dPDB2) :
 				#If both residues belong to the interface, the number of interface shared residues increases
 				if dPDB1[chain][res]["bfactor"] and dPDB2[chain][res]["bfactor"] :
 					nbInter += 1
-					print dPDB1[chain][res]["bfactor"]
-					
+								
 	return nbInter
 
-
-
-
-
+def interfaceHydrophobicity(listBestHits, listBestScores, dico_Rec, thresholdParam, interfaceModeParam):
+	listPercentage=[]
+	for files in listBestHits:
+		dico=parserPDB(files)
+		percentage=int(computeInterfaceScore(dico_Rec, dico, thresholdParam, interfaceModeParam))
+		listPercentage.append(percentage)
+	
+	listnewScore=[(1-a)*b for a,b in zip(listPercentage,listBestScores)]
+	
+	return listnewScore
